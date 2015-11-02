3ba0375711a80e108b2ef40f70d01fa6c22eecd7
BennettIronYard <bennettiy12@gmail.com>
2015-11-02 12:01:15 -0500

    add prepared statement example

--- src/com/instio/Main.java
+++ src/com/instio/Main.java
@@ -14,6 +14,15 @@ public class Main {
         stmt.execute("UPDATE players SET health = 50 WHERE name = 'Alice'");
         stmt.execute("DELETE FROM players WHERE name = 'Bob'");
 
+        //Bad way
+        //String input = "Charlie";
+        //stmt.execute(String.format("INSERT INTO players VALUES('%s', 100, 10, true)", input ));
+        // Good way
+        String input = "Charlie";
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (?, 100, 10, true)");
+        stmt2.setString(1, input);
+        stmt2.execute();
+
         ResultSet results = stmt.executeQuery("SELECT * FROM players");
         while (results.next()) {
             String name = results.getString("name");
